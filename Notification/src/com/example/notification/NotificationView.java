da40be1da926ec3508f8c3c9e3e455b4b724e0e5
aakash <latest2893@gmail.com>
2014-10-29 01:02:32 +0530

    A simple Notification program in Android

---
+++ Notification/src/com/example/notification/NotificationView.java
@@ -0,0 +1,14 @@
+package com.example.notification;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationView extends Activity{
+   @Override
+   public void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.notification);
+   }
+
+}
